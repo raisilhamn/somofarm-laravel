b18203899af268dedb162276ee670136b669c725
rais ilham nustara <rais.ilham.nustara-2020@fst.unair.ac.id>
2022-05-25 14:59:51 +0700

    cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -64,8 +64,8 @@
             <div class="row g-5">
                 <div class="col-md-5 col-lg-4 order-md-last">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
-                        <span class="text-primary">Your cart</span>
-                        <span class="badge bg-primary rounded-pill">3</span>
+                        <span class="text-primary" style="color: #287343">Your cart</span>
+                        <span class="badge rounded-pill">3</span>
                     </h4>
                     <ul class="list-group mb-3">
                         <li class="list-group-item d-flex justify-content-between lh-sm">
