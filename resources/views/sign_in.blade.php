0974936af5610dc2e005413342e346709ee735d0
rais ilham nustara <rais.ilham.nustara-2020@fst.unair.ac.id>
2022-06-03 01:26:15 +0700

    Login berhasil

--- resources/views/sign_in.blade.php
+++ resources/views/sign_in.blade.php
@@ -22,16 +22,23 @@
 <body class="text-center">
 
     <main class="form-signin w-100 m-auto">
-        <form>
+        <form action="/login" method="POST">
+            @csrf
             <img class="mb-4" src="image/somotrans.png" alt="" width="200" height="150">
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <input type="email" class="form-control @error('email') is-invalid @enderror"  id="email"
+                    placeholder="name@example.com" name="email">
                 <label for="floatingInput">Email address</label>
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
 
@@ -41,8 +48,9 @@
                 </label>
             </div>
 
-            <a href="/dashboard">
-                <button class="w-100 btn btn-lg btn-success" type="button" style="background-color: #287343">Sign in</button>
+            <a href="">
+                <button class="w-100 btn btn-lg btn-success" type="submit" style="background-color: #287343">Sign
+                    in</button>
             </a>
 
             <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
