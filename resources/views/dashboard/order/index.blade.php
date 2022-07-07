62c390b65d7eae720f27ef964b0ae2f94133bd7a
nabila indhy <raisilham20@gmail.com>
2022-07-08 04:26:24 +0700

    index

--- resources/views/dashboard/order/index.blade.php
+++ resources/views/dashboard/order/index.blade.php
@@ -20,29 +20,29 @@
     @endif
 
     <div class="table-responsive col-lg-8">
-        <a href="/dashboard/user/create" class="btn btn-primary mb-3">Create New Order</a>
+        <a href="/dashboard/order/create" class="btn btn-primary mb-3">Create New Order</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Password(b5)</th>
+                    <th scope="col">Pesanan</th>
+                    <th scope="col">Jumlah</th>
+                    <th scope="col">Penawaran</th>
                     <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
 
-                @foreach ($items as $i)
+                @foreach ($order as $o)
                     <tr>
-                        <td>{{ $i->name }}</td>
-                        <td>{{ $i->email }}</td>
-                        <td>{{ $i->password }}</td>
-                        <td><a href="/dashboard/user/{{ $i->id }}" class="badge bg-info"><span
+                        <td>{{ $o->nama }}</td>
+                        <td>{{ $o->jumlah }}</td>
+                        <td>{{ $o->penawaran }}</td>
+                        <td><a href="/dashboard/order/{{ $o->id }}" class="badge bg-info"><span
                                     data-feather="eye"></span></a>
-                            <a href="/dashboard/user/{{ $i->id }}/edit" class="badge bg-warning"><span
+                            <a href="/dashboard/order/{{ $o->id }}/edit" class="badge bg-warning"><span
                                     data-feather="edit"></span></a>
 
-                            <form action="/dashboard/user/{{ $i->id }}" method="POST" class="d-inline">
+                            <form action="/dashboard/order/{{ $o->id }}" method="POST" class="d-inline">
                                 @csrf
                                 @method('delete')
                                 <button class="badge bg-danger border-0" onclick="return confirm('Are you sure ?')"><span
